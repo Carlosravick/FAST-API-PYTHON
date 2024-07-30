cc85cc720a8f01af99af18621efc48226a59bd00
Carlos <barra.carlosfilho@hotmail.com>
2024-07-30 16:54:53 -0300

    feature/startfastapi

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Item(BaseModel): 
+    id:int
+    name:str
+    description:str
+    price:int
+    on_offer:bool    
+
+
+app = FastAPI()
+
+
+@app.get('/')   
+def index():
+    return {"messege": "hello World"}
+
+@app.put('/item/{item_id}')
+def update_item(item_id:int, item:Item):
+    return {'name':item.name,
+            'description': item.description,
+            'price': item.price,
+            'on_offer': item.on_offer}
+
+
+@app.get('/greet')
+def greet_optinal_name(name:Optional[str]="user"):
+    return {"messege":f"hello {name}"}
+
+
+
